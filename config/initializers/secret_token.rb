7596f342bf8b5ad1215aec380467eb366b5c0715
gacek47 <gacek47@gmail.com>
2013-03-02 08:33:13 +0100

    pierwszy_probny

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AptRailsProba::Application.config.secret_token = 'e88e3dd537fc5c26863e66f5a07660f22f3663cd04c87f8714734ba2ffec6d7042a1388cee15c95f879defd25e160b440a31260a2a016a7cf65e6925bc62d786'
